de7ac2acfe831f92f53127590dcec1b7b9112a66
Paolo Di Giglio <digiglio@lab3d.com>
2023-02-21 19:33:41 +0100

    Split tests

--- main.cpp
+++ main.cpp
@@ -20,6 +20,49 @@ namespace
 	template <typename...>
 	void foo();
 
+	struct TestEbo
+	{
+		void value_categories()
+		{
+			using tr::detail::ebo;
+
+			struct tag_t {};
+			struct S {};
+			ebo<S, tag_t, true> ec{};
+			ebo<S, tag_t, false> e{};
+
+			static_assert(
+				std::is_same_v<decltype(get_ebo_val(ec)), decltype(get_ebo_val(e))>);
+
+			static_assert(std::is_same_v<decltype(get_ebo_val(std::as_const(ec))),
+				decltype(get_ebo_val(std::as_const(e)))>);
+
+			static_assert(std::is_same_v<decltype(get_ebo_val(std::move(ec))),
+				decltype(get_ebo_val(std::move(e)))>);
+
+			static_assert(
+				std::is_same_v<decltype(get_ebo_val(std::move(std::as_const(ec)))),
+				decltype(get_ebo_val(std::move(std::as_const(e))))>);
+		}
+
+		void traits()
+		{
+			using tr::detail::ebo;
+			using tr::detail::ebo_traits;
+
+			struct tag_t {};
+			struct S {};
+
+			struct S_final final {};
+			using func_ptr_t = void (*)();
+			static_assert(ebo_traits<ebo<S, tag_t>>::is_compressed);
+			static_assert(!ebo_traits<ebo<S_final, tag_t>>::is_compressed);
+			static_assert(!ebo_traits<ebo<S&, tag_t>>::is_compressed);
+			static_assert(!ebo_traits<ebo<int, tag_t>>::is_compressed);
+			static_assert(!ebo_traits<ebo<func_ptr_t, tag_t>>::is_compressed);
+		}
+	};
+
     // Check that class tr::detail::ebo behaves like I expect. If this function
     // compiles, the tests pass.
 	void test_ebo()
@@ -56,41 +99,66 @@ namespace
 	}
 
     // Check that class tr::combinator behaves like I expect.
-	void test_combinator()
+	struct TestCombinator
 	{
-		using tr::combinator;
-		constexpr std::tuple vals{ 0.1, 1, 2, 3, 4 };
-
+		void apply()
 		{
-			constexpr combinator minComb{ [](auto i, auto j) { return i < j ? i : j; } };
-			static_assert(sizeof(minComb) == 1);
-			static_assert(std::is_aggregate_v<decltype(minComb)>);
+			using tr::combinator;
+			constexpr std::tuple vals{ 0.1, 1, 2, 3, 4 };
 
-			constexpr auto minVal = std::apply(
-				[=](auto ... args) { return (minComb | ... | args)(); },
-				vals);
+			{
+				constexpr combinator minComb{ [](auto i, auto j) { return i < j ? i : j; } };
+				static_assert(sizeof(minComb) == 1);
+				static_assert(std::is_aggregate_v<decltype(minComb)>);
 
-			static_assert(minVal == std::get<0>(vals));
+				constexpr auto minVal = std::apply(
+					[=](auto ... args) { return (minComb | ... | args)(); },
+					vals);
+
+				static_assert(minVal == std::get<0>(vals));
+			}
+
+			{
+				constexpr combinator maxComb{ [](auto i, auto j) { return i > j ? i : j; }, 5 };
+				static_assert(sizeof(maxComb) == sizeof(int));
+				static_assert(std::is_aggregate_v<decltype(maxComb)>);
+
+				constexpr auto max = std::apply(
+					[=](auto ... args) { return (maxComb | ... | args)(); },
+					vals);
+
+				constexpr auto init = maxComb();
+				static_assert(max == init);
+			}
 		}
 
+		void value_categories()
 		{
-			constexpr combinator maxComb{ [](auto i, auto j) { return i > j ? i : j; }, 5 };
-			static_assert(sizeof(maxComb) == sizeof(int));
-			static_assert(std::is_aggregate_v<decltype(maxComb)>);
+			using tr::combinator;
 
-			constexpr auto max = std::apply(
-				[=](auto ... args) { return (maxComb | ... | args)(); },
-				vals);
+			{
+				combinator comb{ [](int, int) {} };
+				static_assert(
+					std::is_rvalue_reference_v<decltype(std::move(comb).get_operator())> &&
+					!std::is_const_v<std::remove_reference_t<decltype(std::move(comb).get_operator())>>);
 
-			constexpr auto init = maxComb();
-			static_assert(max == init);
+				static_assert(
+					std::is_rvalue_reference_v<decltype(std::move(std::as_const(comb)).get_operator())> &&
+					std::is_const_v<std::remove_reference_t<decltype(std::move(std::as_const(comb)).get_operator())>>);
+
+				static_assert(
+					std::is_lvalue_reference_v<decltype(comb.get_operator())> &&
+					!std::is_const_v<std::remove_reference_t<decltype(comb.get_operator())>>);
+
+				static_assert(
+					std::is_lvalue_reference_v<decltype(std::as_const(comb).get_operator())> &&
+					std::is_const_v<std::remove_reference_t<decltype(std::as_const(comb).get_operator())>>);
+			}
 		}
-	}
+	};
 }
 
 int main()
 {
-	test_ebo();
-	test_combinator();
 }
 
