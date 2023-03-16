693bb28800aa70d134b21223ba4c619621e185ec
Paolo Di Giglio <digiglio@lab3d.com>
2023-03-16 18:03:00 +0100

    Refactor CMakeLists.txt; improve `overload`
     * `overload` can now also wrap function pointers;
     * Create a test/ folder;
     * Add a test file for `overload`.

--- tr/overload.h
+++ tr/overload.h
@@ -1,13 +1,30 @@
 #pragma once
 
-#include "detail/type_traits.h"
+#include <type_traits>
+#include <utility>
 
 namespace tr {
+
+namespace detail {
+template <typename Callable>
+struct CallableWrapper : Callable {};
+
+template <typename R, typename... Args>
+struct CallableWrapper<R (*)(Args...)> {
+    R (*Func_)(Args...);
+
+    constexpr R operator()(Args... args) const {
+        return this->Func_(std::forward<Args>(args)...);
+    }
+};
+} // namespace detail
+
 template <typename... Callables>
-struct overload : Callables... {
-    using Callables::operator()...;
+struct overload : detail::CallableWrapper<Callables>... {
+    using detail::CallableWrapper<Callables>::operator()...;
 };
 
 template <typename... Callables>
-overload(Callables &&...) -> overload<detail::remove_cvref_t<Callables>...>;
+overload(Callables &&...) -> overload<std::decay_t<Callables>...>;
+
 } // namespace tr
