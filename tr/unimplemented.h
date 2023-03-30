90c084b4a71bde2445e10670862486ade3915803
Paolo Di Giglio <digiglio@lab3d.com>
2023-03-30 18:10:08 +0200

    unimplemented: add is_unimplemented<> trait

--- tr/unimplemented.h
+++ tr/unimplemented.h
@@ -1,18 +1,20 @@
 #pragma once
 
-//#include <type_traits>
+#include <type_traits>
 
 namespace tr {
 
 /// @brief Base class for unimplemented tag-dispatched function objects.
 struct unimplemented {};
 
-// template <typename, typename = void>
-// struct is_implemented : std::false_type {};
-//
-// template <typename T>
-// struct is_implemented<T, std::enable_if_t<std::is_base_of_v<unimplemented,
-// T>>>
-//    : std::true_type {};
+template <typename, typename = void>
+struct is_implemented : std::false_type {};
+
+template <typename T>
+struct is_implemented<T, std::enable_if_t<!std::is_base_of_v<unimplemented, T>>>
+    : std::true_type {};
+
+template <typename T>
+static constexpr bool is_implemented_v{is_implemented<T>::value};
 
 } // namespace tr
