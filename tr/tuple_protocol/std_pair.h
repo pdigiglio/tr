ac1d20008858b65746d38f0a4b0322702bed2f64
Paolo Di Giglio <digiglio@lab3d.com>
2023-03-31 23:21:33 +0200

    Specialize at_impl, length_impl for std::pair

--- tr/tuple_protocol/std_pair.h
+++ tr/tuple_protocol/std_pair.h
@@ -1,10 +1,33 @@
 #pragma once
 
+#include "../fwd/at.h"
+#include "../fwd/length.h"
+
 #include "../tuple_protocol.h"
 
 #include <utility>
 
 namespace tr {
+
+template <typename First, typename Second>
+struct length_impl<std::pair<First, Second>> {
+    template <typename Pair>
+    [[nodiscard]] constexpr static auto apply(Pair &&) noexcept
+        -> value_constant<2> {
+        return {};
+    }
+};
+
+template <typename First, typename Second>
+struct at_impl<std::pair<First, Second>> {
+
+    template <typename Pair, typename Idx>
+    [[nodiscard]] constexpr static auto apply(Pair &&pair, Idx) noexcept
+        -> decltype(auto) {
+        return std::get<Idx{}>(std::forward<Pair>(pair));
+    }
+};
+
 template <typename... Ts>
 struct tup_size<std::pair<Ts...>> : std::tuple_size<std::pair<Ts...>> {};
 
