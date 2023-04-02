7f9d31dc7b713bbf89df5bc4c7e5dcde98696a78
Paolo Di Giglio <digiglio@lab3d.com>
2023-04-02 18:48:42 +0200

    std_integer_sequence.h: trailing returns + formatting

--- tr/tuple_protocol/std_integer_sequence.h
+++ tr/tuple_protocol/std_integer_sequence.h
@@ -3,8 +3,8 @@
 #include "../fwd/at.h"
 #include "../fwd/length.h"
 
-#include "../value_constant.h"
 #include "../tuple_protocol.h"
+#include "../value_constant.h"
 
 #include <cstddef>
 #include <utility>
@@ -15,7 +15,7 @@ template <typename T, T... Is>
 struct at_impl<std::integer_sequence<T, Is...>> {
 
     template <typename Iterable, typename Idx>
-    static constexpr decltype(auto) apply(Iterable &&, Idx) noexcept {
+    static constexpr auto apply(Iterable &&, Idx) noexcept -> decltype(auto) {
         static_assert(Idx{} < sizeof...(Is), "Index out of bounds");
         constexpr T is[]{Is...};
         return value_c<is[Idx{}]>;
@@ -26,17 +26,18 @@ template <typename T, T... Is>
 struct length_impl<std::integer_sequence<T, Is...>> {
 
     template <typename Sized>
-    static constexpr value_constant<sizeof...(Is)> apply(Sized &&) noexcept {
+    static constexpr auto apply(Sized &&) noexcept
+        -> value_constant<sizeof...(Is)> {
         return {};
     }
 };
 
 // TODO: remove this --
- template <typename T, T... Is>
- struct tup_size<std::integer_sequence<T, Is...>>
+template <typename T, T... Is>
+struct tup_size<std::integer_sequence<T, Is...>>
     : std::integral_constant<std::size_t, sizeof...(Is)> {};
 
- template <std::size_t J, typename T, T... Is>
+template <std::size_t J, typename T, T... Is>
 [[nodiscard]] constexpr auto get(std::integer_sequence<T, Is...>) noexcept {
     static_assert(J < sizeof...(Is), "Index out of bounds");
     constexpr std::size_t indices[]{Is...};
