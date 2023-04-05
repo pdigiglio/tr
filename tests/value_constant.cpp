08368c8dd08c28bede5bb1594964183aa649f03c
Paolo Di Giglio <digiglio@lab3d.com>
2023-04-05 18:37:38 +0200

    value_constant: operators are SFINAE friendly

--- tests/value_constant.cpp
+++ tests/value_constant.cpp
@@ -1,14 +1,65 @@
 #include <tr/value_constant.h>
 
-#include <climits>
+#include <tr/detail/type_traits.h>
 #include <tr/type_constant.h>
+
+#include <climits>
+#include <cstdint>
 
 using tr::false_c;
 using tr::true_c;
 using tr::type_c;
 using tr::value_c;
+using tr::detail::check_expr;
+
+#define CHECK_BINARY_EXPR(OP)                                                  \
+    check_expr([](auto lhs, auto rhs) -> decltype(lhs OP rhs) {})
+
+#define CHECK_UNARY_EXPR(OP) check_expr([](auto arg) -> decltype(OP arg) {})
 
 namespace {
+
+enum struct dummy_enum { zero };
+
+enum struct bitmask_enum : std::uint8_t {
+    zero = 0,
+    one = (1 << 0),
+    two = (1 << 1),
+    three = one | two,
+    four = (1 << 2),
+    max = static_cast<std::uint8_t>(-1)
+};
+
+#define BITMASK_BITWISE_OP(ENUM, OP)                                           \
+    constexpr ENUM operator OP(ENUM lhs, ENUM rhs) {                           \
+        using underlying_t = std::underlying_type_t<ENUM>;                     \
+        return static_cast<ENUM>(static_cast<underlying_t>(lhs)                \
+                                     OP static_cast<underlying_t>(rhs));       \
+    }
+
+BITMASK_BITWISE_OP(bitmask_enum, &)
+BITMASK_BITWISE_OP(bitmask_enum, |)
+BITMASK_BITWISE_OP(bitmask_enum, ^)
+
+#undef BITMASK_BITWISE_OP
+
+#define BITMASK_SHIFT_OP(ENUM, OP)                                             \
+    template <typename Int>                                                    \
+    constexpr auto operator OP(ENUM e, Int shift)                              \
+        ->std::enable_if_t<std::is_integral_v<Int>, ENUM> {                    \
+        using underlying_t = std::underlying_type_t<ENUM>;                     \
+        return static_cast<ENUM>(static_cast<underlying_t>(e) OP shift);       \
+    }
+
+BITMASK_SHIFT_OP(bitmask_enum, <<)
+BITMASK_SHIFT_OP(bitmask_enum, >>)
+
+#undef BITMASK_SHIFT_OP
+
+constexpr bitmask_enum operator~(bitmask_enum e) noexcept {
+    using underlying_t = std::underlying_type_t<bitmask_enum>;
+    return static_cast<bitmask_enum>(~static_cast<underlying_t>(e));
+}
 
 template <typename UInt0, typename Uint1>
 constexpr UInt0 pow(UInt0 base, Uint1 exp) noexcept {
@@ -65,6 +116,79 @@ struct TestValueConstant {
         static_assert(~a == -(a + 1));
         static_assert(+a == a);
     }
+
+    void test_sfinae() {
+
+        {
+            auto zero = value_c<bitmask_enum::zero>;
+            auto one = value_c<bitmask_enum::one>;
+            auto two = value_c<bitmask_enum::two>;
+            auto three = value_c<bitmask_enum::three>;
+            auto four = value_c<bitmask_enum::four>;
+            auto max = value_c<bitmask_enum::max>;
+
+            static_assert((one & two) == zero);
+            static_assert((one ^ three) == two);
+            static_assert((one | two) == three);
+            static_assert(~zero == max);
+
+            static_assert((two >> value_c<1>) == one);
+            static_assert((two << value_c<1>) == four);
+
+// MSVC allows all of the meaningless constexpr calculations below.
+#ifndef _MSC_VER
+            static_assert(!CHECK_BINARY_EXPR(<<)(one, value_c<-1>));
+            static_assert(!CHECK_BINARY_EXPR(>>)(one, value_c<-1>));
+#endif // _MSC_VER
+        }
+
+        {
+// MSVC allows all of the meaningless constexpr calculations below.
+#ifndef _MSC_VER
+            static_assert(!CHECK_BINARY_EXPR(/)(value_c<1>, value_c<0>));
+            static_assert(CHECK_BINARY_EXPR(/)(value_c<1>, value_c<1>));
+
+            static_assert(!CHECK_BINARY_EXPR(%)(value_c<1>, value_c<0>));
+            static_assert(CHECK_BINARY_EXPR(%)(value_c<1>, value_c<1>));
+
+            // A dummy enum with that doesn't support operators.
+
+            auto a = value_c<dummy_enum{}>;
+            auto b = value_c<1>;
+            static_assert(!CHECK_BINARY_EXPR(==)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(!=)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(<)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(<=)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(>)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(>=)(a, b));
+
+            static_assert(!CHECK_BINARY_EXPR(+)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(-)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(*)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(/)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(%)(a, b));
+
+            // Logical
+            static_assert(!CHECK_BINARY_EXPR(&&)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(||)(a, b));
+
+            // Bitwise
+            static_assert(!CHECK_BINARY_EXPR(&)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(|)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(^)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(<<)(a, b));
+            static_assert(!CHECK_BINARY_EXPR(>>)(a, b));
+
+            // UNARY
+            static_assert(!CHECK_UNARY_EXPR(-)(a));
+            static_assert(!CHECK_UNARY_EXPR(~)(a));
+            static_assert(!CHECK_UNARY_EXPR(+)(a));
+#endif // _MSC_VER
+        }
+    }
 };
 
 } // namespace
+
+#undef CHECK_BINARY_EXPR
+#undef CHECK_UNARY_EXPR
--- tr/value_constant.h
+++ tr/value_constant.h
@@ -5,46 +5,75 @@
 
 namespace tr {
 
+namespace detail {
+
+template <template <auto...> typename, typename, auto...>
+struct is_valid_value_expr : std::false_type {};
+
+template <template <auto...> typename ValueExpr, auto... Vals>
+struct is_valid_value_expr<ValueExpr, std::void_t<ValueExpr<Vals...>>, Vals...>
+    : std::true_type {};
+
+} // namespace detail
+
+template <template <auto...> typename ValueExpr, auto... Vals>
+static constexpr bool is_valid_value_expr_v{
+    detail::is_valid_value_expr<ValueExpr, void, Vals...>::value};
+
+
 template <auto Val>
 struct value_constant {
     using type = decltype(Val);
     static constexpr type value{Val};
 
+    /// @brief Implicit cast to the wrapped value.
+    /// @return The wrapped value.
     [[nodiscard]] constexpr operator type() const noexcept { return Val; }
 
+    /// @brief Explicit cast to the wrapped value.
+    /// @tparam T The type to cast to (it's different from the wrapped value type).
+    /// @return The wrapped value.
     template <typename T, typename = std::enable_if_t<!std::is_same_v<T, type>>>
     [[nodiscard]] constexpr explicit operator T() const noexcept {
         return T{Val};
         //     ^ prevent narrowing conversions (on explicit casts).
     }
 
-    // TODO: make the operators SFINAE friendly.
-
-#define DEFINE_INFIX_BINARY_OPERATOR(OP)                                       \
+#define DEFINE_INFIX_BINARY_OPERATOR(OP, EXPR_TYPE_NAME)                       \
+  private:                                                                     \
+    template <auto Val_>                                                       \
+    using EXPR_TYPE_NAME = value_constant<(Val OP Val_)>;                      \
+                                                                               \
+  public:                                                                      \
     template <auto Val_>                                                       \
     [[nodiscard]] friend constexpr auto operator OP(                           \
-        value_constant, value_constant<Val_>) noexcept {                       \
-        return value_constant<(Val OP Val_)>{};                                \
-    }
+        value_constant, value_constant<Val_>) noexcept->EXPR_TYPE_NAME<Val_> { \
+        return {};                                                             \
+    }                                                                          \
+                                                                               \
+    template <auto Val_>                                                       \
+    friend auto operator OP(value_constant, value_constant<Val_>)              \
+        ->std::enable_if_t<!is_valid_value_expr_v<EXPR_TYPE_NAME, Val_>> =     \
+        delete; \
 
     // Comparators
-    DEFINE_INFIX_BINARY_OPERATOR(==)
-    DEFINE_INFIX_BINARY_OPERATOR(!=)
-    DEFINE_INFIX_BINARY_OPERATOR(<)
-    DEFINE_INFIX_BINARY_OPERATOR(<=)
-    DEFINE_INFIX_BINARY_OPERATOR(>)
-    DEFINE_INFIX_BINARY_OPERATOR(>=)
+    DEFINE_INFIX_BINARY_OPERATOR(==, equal_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(!=, not_equal_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(<, less_than_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(<=, less_or_equal_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(>, greater_than_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(>=, greater_or_equal_expr_t)
 
     // Arithmetic
-    DEFINE_INFIX_BINARY_OPERATOR(+)
-    DEFINE_INFIX_BINARY_OPERATOR(-)
-    DEFINE_INFIX_BINARY_OPERATOR(*)
-    DEFINE_INFIX_BINARY_OPERATOR(/)
-    DEFINE_INFIX_BINARY_OPERATOR(%)
+    DEFINE_INFIX_BINARY_OPERATOR(+, plus_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(-, minus_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(*, multiplication_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(/, division_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(%, modulo_expr_t)
 
     // Logical
-    DEFINE_INFIX_BINARY_OPERATOR(&&)
-    DEFINE_INFIX_BINARY_OPERATOR(||)
+    DEFINE_INFIX_BINARY_OPERATOR(&&, logic_and_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(||, logic_or_expr_t)
 
     // Question to self: Do logical operators need to short-circuit?
     //
@@ -52,32 +81,50 @@ struct value_constant {
     // that has no side effects and has no runtime cost.
 
     // Bitwise
-    DEFINE_INFIX_BINARY_OPERATOR(&)
-    DEFINE_INFIX_BINARY_OPERATOR(|)
-    DEFINE_INFIX_BINARY_OPERATOR(^)
-    DEFINE_INFIX_BINARY_OPERATOR(<<)
-    DEFINE_INFIX_BINARY_OPERATOR(>>)
+    DEFINE_INFIX_BINARY_OPERATOR(&, bitwise_and_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(|, bitwise_or_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(^, bitwise_xor_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(<<, left_shift_expr_t)
+    DEFINE_INFIX_BINARY_OPERATOR(>>, right_shift_expr_t)
 
 #undef DEFINE_INFIX_BINARY_OPERATOR
+
 };
 
-#define DEFINE_PREFIX_UNARY_OPERATOR(OP)                                       \
+#define DEFINE_PREFIX_UNARY_OPERATOR(OP, EXPR_TYPE_NAME)                       \
+    namespace detail {                                                         \
     template <auto Val>                                                        \
-    [[nodiscard]] constexpr auto operator OP(value_constant<Val>) noexcept {   \
-        return value_constant<(OP Val)>{};                                     \
-    }
+    using EXPR_TYPE_NAME = value_constant<(OP Val)>;                           \
+    }                                                                          \
+                                                                               \
+    template <auto Val>                                                        \
+    [[nodiscard]] constexpr auto operator OP(                                  \
+        value_constant<Val>) noexcept->detail::EXPR_TYPE_NAME<Val> {           \
+        return {};                                                             \
+    }                                                                          \
+                                                                               \
+    template <auto Val>                                                        \
+    auto operator OP(value_constant<Val>)                                      \
+        ->std::enable_if_t<                                                    \
+            !is_valid_value_expr_v<detail::EXPR_TYPE_NAME, Val>> = delete;
 
-DEFINE_PREFIX_UNARY_OPERATOR(!)
-DEFINE_PREFIX_UNARY_OPERATOR(~)
-DEFINE_PREFIX_UNARY_OPERATOR(+)
-DEFINE_PREFIX_UNARY_OPERATOR(-)
+DEFINE_PREFIX_UNARY_OPERATOR(!, value_constant_logical_not_expr_t)
+DEFINE_PREFIX_UNARY_OPERATOR(~, value_constant_bitwise_not_expr_t)
+DEFINE_PREFIX_UNARY_OPERATOR(+, value_constant_unary_plus_expr_t)
+DEFINE_PREFIX_UNARY_OPERATOR(-, value_constant_unary_minus_expr_t)
 
 #undef DEFINE_PREFIX_UNARY_OPERATOR
 
+/// @brief A variable template to simplify the creation of
+/// `value_constant<Val>` instances.
+/// @tparam Val The wrapped value.
 template <auto Val>
 static constexpr value_constant<Val> value_c{};
 
+/// @brief A variable template to wrap `true`.
 static constexpr auto true_c = value_c<true>;
+
+/// @brief A variable template to wrap `false`.
 static constexpr auto false_c = value_c<false>;
 
 template <unsigned Val>
