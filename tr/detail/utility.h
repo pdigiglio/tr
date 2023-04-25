f20dde680e46c18678ae1b163a4d1716f4a9f6ac
Paolo Di Giglio <digiglio@lab3d.com>
2023-04-25 22:41:49 +0200

    Add _c, _ic, _zuic, _uic user-defined literal suffixes

--- tr/detail/utility.h
+++ tr/detail/utility.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "./literal_parser.h"
+
 #include <type_traits>
 #include <utility>
 
@@ -28,4 +30,41 @@ constexpr std::integral_constant<int, I> ic{};
 
 template <std::size_t I>
 constexpr std::integral_constant<std::size_t, I> zuic{};
+
+namespace literals {
+
+/// @brief User-defined literal operator for `tr::integral_constant<std::size_t, _>`,
+/// where `_` is the parsed literal value.
+template <char... Chars>
+[[nodiscard]] constexpr auto operator""_zuic() noexcept
+    -> detail::integral_parser_t<std::size_t, Chars...> {
+    return {};
+}
+
+/// @brief User-defined literal operator for `tr::integral_constant<unsigned, _>`,
+/// where `_` is the parsed literal value.
+template <char... Chars>
+[[nodiscard]] constexpr auto operator""_uic() noexcept
+    -> detail::integral_parser_t<unsigned, Chars...> {
+    return {};
+}
+
+/// @brief User-defined literal operator for `tr::integral_constant<int, _>`,
+/// where `_` is the parsed literal value.
+template <char... Chars>
+[[nodiscard]] constexpr auto operator""_ic() noexcept
+    -> detail::integral_parser_t<int, Chars...> {
+    return {};
+}
+
+/// @brief User-defined literal operator for `tr::integral_constant<int, _>`,
+/// where `_` is the parsed literal value.
+template <char... Chars>
+[[nodiscard]] constexpr auto operator""_c() noexcept
+    -> detail::integral_parser_t<int, Chars...> {
+    return {};
+}
+
+} // namespace literals
+
 } // namespace tr
