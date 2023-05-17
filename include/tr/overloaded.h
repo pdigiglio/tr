4b76d26669a76f39d36022040e03f93c2f53302e
Paolo Di Giglio <digiglio@lab3d.com>
2023-05-17 23:44:35 +0200

    overloaded: support more callables, improve docs
    Added support for:
     * non-const member functions: R (C::*)(Args...);
     * const member functions: R (C::*)(Args...) const;
     * l/r-reference const/non-const member function overloads;
     * pointer to member variables: R C::*.

--- include/tr/overloaded.h
+++ include/tr/overloaded.h
@@ -5,15 +5,172 @@
 namespace tr {
 
 namespace detail {
+
+/// @brief Primary template.
+/// @tparam Callable A class type with a call operator.
 template <typename Callable>
 struct CallableWrapper : Callable {};
 
+/// @brief Specialization for function pointers.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
 template <typename R, typename... Args>
 struct CallableWrapper<R (*)(Args...)> {
     R (*Func_)(Args...);
 
-    constexpr R operator()(Args... args) const {
+    constexpr auto operator()(Args... args) const -> R {
         return this->Func_(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to non-const member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...)> {
+    R (C::*Func_)(Args...);
+
+    constexpr auto operator()(C &c, Args... args) const /* noexcept(?) */ -> R {
+        return (c.*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C &&c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (static_cast<C &&>(c).*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C *c, Args... args) const /* noexcept(?) */ -> R {
+        return (c->*this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to const member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function arguments types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...) const> {
+    R (C::*Func_)(Args...) const;
+
+    constexpr auto operator()(C const &c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c.*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C const &&c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (static_cast<C &&>(c).*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C const *c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c->*this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to non-const l-value member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...) &> {
+    R (C::*Func_)(Args...) &;
+
+    constexpr auto operator()(C &c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c.*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C *c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c->*this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to const l-value member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...) const &> {
+    R (C::*Func_)(Args...) const &;
+
+    constexpr auto operator()(C const &c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c.*this->Func_)(static_cast<Args>(args)...);
+    }
+
+    constexpr auto operator()(C const *c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (c->*this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to non-const r-value member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...) &&> {
+    R (C::*Func_)(Args...) &&;
+
+    constexpr auto operator()(C &&c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (static_cast<C &&>(c).*this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization for pointer to const r-value member functions.
+/// @tparam R The function return type.
+/// @tparam ...Args The function argument types.
+/// @tparam C The class type.
+template <typename R, typename C, typename... Args>
+struct CallableWrapper<R (C::*)(Args...) const &&> {
+    R (C::*Func_)(Args...) const &&;
+
+    constexpr auto operator()(C const &&c, Args... args) const
+        /* noexcept(?) */ -> R {
+        return (static_cast<C const &&>(c).*
+                this->Func_)(static_cast<Args>(args)...);
+    }
+};
+
+/// @brief Specialization to pointer to member variable.
+/// @tparam R The member variable type.
+/// @tparam C The class type.
+template <typename R, typename C>
+struct CallableWrapper<R C::*> {
+    R C::*MemPtr_;
+
+    constexpr auto operator()(C &c) const
+        /* noexcept(?) */ -> R & {
+        return (c.*this->MemPtr_);
+    }
+
+    constexpr auto operator()(C const &c) const
+        /* noexcept(?) */ -> R const & {
+        return (c.*this->MemPtr_);
+    }
+
+    constexpr auto operator()(C &&c) const
+        /* noexcept(?) */ -> R && {
+        return (static_cast<C &&>(c).*this->MemPtr_);
+    }
+
+    constexpr auto operator()(C const &&c) const
+        /* noexcept(?) */ -> R const && {
+        return (static_cast<C const &&>(c).*this->MemPtr_);
+    }
+
+    constexpr auto operator()(C *c) const
+        /* noexcept(?) */ -> R & {
+        return (c->*this->MemPtr_);
+    }
+
+    constexpr auto operator()(C const *c) const
+        /* noexcept(?) */ -> R const & {
+        return (c->*this->MemPtr_);
     }
 };
 
@@ -21,16 +178,16 @@ struct CallableWrapper<R (*)(Args...)> {
 
 /// @brief A class to compose a number of `Callables...` and provide an
 /// overloaded call operator.
-/// 
+///
 /// @details The call-operator overload set is the union of the overload sets in
 /// each of the `Callables...`, e.g.:
-/// 
+///
 /// @code
 /// int f(int);
-/// 
+///
 /// // o0 has two call-operator overloads (i.e. for int, double)
 /// overloaded o0 {f, [](double) { /* ... */ }};
-/// 
+///
 /// // o1 has three call-operator overloads (i.e. for int, double, char const*)
 /// overloaded o1 {o0, [](char const*) { /* ... */ }};
 /// @endcode
--- tests/overloaded.cpp
+++ tests/overloaded.cpp
@@ -1,34 +1,40 @@
 #include <tr/overloaded.h>
+
+#include <tr/detail/type_traits.h>
 #include <tr/type_constant.h>
 #include <tr/value_constant.h>
 
+#include <utility>
+#include <functional>
+
 using tr::overloaded;
+using tr::true_c;
 using tr::type_c;
 using tr::value_c;
-using tr::true_c;
 
 namespace {
 
 template <typename Overload>
-constexpr auto is_aggregate(Overload) noexcept {
-    return value_c<std::is_aggregate_v<Overload>>;
+constexpr auto is_aggregate(Overload &&) noexcept -> bool {
+    using overload_t = tr::detail::remove_cvref_t<Overload>;
+    return std::is_aggregate_v<overload_t>;
 }
 
-int foo(int) { return {}; }
+auto foo(int) -> tr::type_constant<int> { return {}; }
 
 struct TestOverload {
     void test_lambdas() {
         // Check the call operator is constant evaluated, if the lambdas I
         // pass in can be constant evaluated
-        overloaded ovrld{[](auto) { return 0; }, [](int) { return 1; },
-                       [](char const *) { return 2; }};
+        overloaded ovrld{[](auto) { return value_c<0>; },
+                         [](int) { return value_c<1>; },
+                         [](char const *) { return value_c<2>; }};
 
         static_assert(ovrld(1.0) == 0);
         static_assert(ovrld(1) == 1);
         static_assert(ovrld("") == 2);
 
-        auto const isAgg = is_aggregate(ovrld);
-        static_assert(isAgg == true_c);
+        static_assert(is_aggregate(ovrld));
     }
 
     void test_func_ptr() {
@@ -38,16 +44,92 @@ struct TestOverload {
 
         // Check I can mix lambdas, callable objects and function pointers.
         overloaded ovrld{[](auto) { return -1; }, Callable{}, foo};
+        static_assert(is_aggregate(ovrld));
 
         static_assert(ovrld("") == -1);
         static_assert(ovrld(42.0) == 1.0);
 
-        // foo is not constexpr
-        //static_assert(ovrld(42) == 0);
-        static_assert(type_c<decltype(ovrld(42))> == type_c<int>);
+        // Note: ovrld(42) is not constexpr because foo is not.
+        auto int_c = ovrld(42);
+        static_assert(int_c == type_c<int>);
+    }
 
-        auto const isAgg = is_aggregate(ovrld);
-        static_assert(isAgg == true_c);
+    void test_std_function() {
+        std::function func{foo};
+        overloaded o{func};
+        auto res = o(0);
+        static_assert(res == type_c<int>);
+    }
+
+    void test_mem_func_ptr() {
+        struct S {
+            auto foo() & -> tr::type_constant<S &> { return {}; }
+            auto foo() && -> tr::type_constant<S &&> { return {}; }
+            auto foo() const & -> tr::type_constant<S const &> { return {}; }
+            auto foo() const && -> tr::type_constant<S const &&> { return {}; }
+
+            auto bar() -> tr::type_constant<S> { return {}; }
+            auto bar() const -> tr::type_constant<S const> { return {}; }
+        };
+
+        using foo_ptr_t = tr::type_constant<S> (S::*)();
+        using cfoo_ptr_t = tr::type_constant<S const> (S::*)() const;
+
+        using lref_foo_ptr_t = tr::type_constant<S &> (S::*)() &;
+        using rref_foo_ptr_t = tr::type_constant<S &&> (S::*)() &&;
+        using clref_foo_ptr_t = tr::type_constant<S const &> (S::*)() const &;
+        using crref_foo_ptr_t = tr::type_constant<S const &&> (S::*)() const &&;
+
+        S s;
+
+        {
+            overloaded o{(lref_foo_ptr_t)&S::foo, (rref_foo_ptr_t)&S::foo,
+                         (clref_foo_ptr_t)&S::foo, (crref_foo_ptr_t)&S::foo};
+
+            auto s_lref = o(s);
+            static_assert(s_lref == type_c<S &>);
+
+            auto s_clref = o(std::as_const(s));
+            static_assert(s_clref == type_c<S const &>);
+
+            auto s_rref = o(std::move(s));
+            static_assert(s_rref == type_c<S &&>);
+
+            auto s_crref = o(std::move(std::as_const(s)));
+            static_assert(s_crref == type_c<S const &&>);
+        }
+
+        {
+            overloaded o{(foo_ptr_t)&S::bar, (cfoo_ptr_t)&S::bar};
+
+            auto s_non_const = o(s);
+            static_assert(s_non_const == type_c<S>);
+
+            auto s_const = o(std::as_const(s));
+            static_assert(s_const == type_c<S const>);
+        }
+    }
+
+    void test_mem_ptr() {
+        struct S {
+            int i{};
+        };
+
+        overloaded o{&S::i};
+
+        S s;
+
+        using int_lref_t = decltype(o(s));
+        static_assert(std::is_same_v<int_lref_t, int &>);
+
+        using int_clref_t = decltype(o(std::as_const(s)));
+        static_assert(std::is_same_v<int_clref_t, int const &>);
+
+        using int_rref_t = decltype(o(std::move(s)));
+        static_assert(std::is_same_v<int_rref_t, int &&>);
+
+        using int_crref_t = decltype(o(std::move(std::as_const(s))));
+        static_assert(std::is_same_v<int_crref_t, int const &&>);
     }
 
     void test_compose() {
@@ -56,13 +138,12 @@ struct TestOverload {
         overloaded o1{[](double) { return 1; }, [](char const *) { return 2; }};
 
         overloaded ovrld{o0, o1, [](auto) { return 3; }};
+        static_assert(is_aggregate(ovrld));
+
         static_assert(ovrld(42) == 0);
         static_assert(ovrld(0.) == 1);
         static_assert(ovrld("") == 2);
         static_assert(ovrld(1u) == 3);
-
-        auto const isAgg = is_aggregate(ovrld);
-        static_assert(isAgg == true_c);
     }
 };
 
