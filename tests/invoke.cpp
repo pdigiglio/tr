c8a0af20d57ef88df264c406a6fc423dcb8f0707
Paolo Di Giglio <digiglio@lab3d.com>
2023-05-23 19:16:03 +0200

    invoke: add some more test

--- tests/invoke.cpp
+++ tests/invoke.cpp
@@ -1,11 +1,19 @@
 #include <tr/invoke.h>
 
 #include <tr/type_constant.h>
+#include <tr/type_pack.h>
 
 #include <functional>
 #include <type_traits>
 
 namespace {
+
+struct FwdToTypePack {
+    template <typename... Args>
+    auto operator()(Args &&...) const noexcept -> tr::type_pack<Args &&...> {
+        return {};
+    }
+};
 
 struct NoCopyNorMove {
     NoCopyNorMove() = default;
@@ -19,7 +27,7 @@ struct Func {
     Func(Func &&) = delete;
     Func(Func const &) = delete;
 
-    constexpr auto operator()(int i) & {
+    constexpr auto operator()(int i = 0) & {
         if (i)
             throw 0;
         return tr::type_c<Func &>;
@@ -57,6 +65,33 @@ struct Func {
 
 auto foo_may_throw(int) -> tr::type_constant<int> { return {}; }
 auto foo_noexcept(int) noexcept -> tr::type_constant<int> { return {}; }
+
+template <typename ExpectedResT, bool ExpectedNoExcept, typename Callable,
+          typename Obj>
+void test_member_ptr(Callable callable, Obj &&obj) {
+
+    {
+        using tr::invoke;
+
+        auto res = invoke(callable, static_cast<Obj &&>(obj));
+        static_assert(std::is_same_v<decltype(res), ExpectedResT>,
+                      "result type is not the expected one");
+
+        constexpr bool nx{noexcept(invoke(callable, static_cast<Obj &&>(obj)))};
+        static_assert(nx == ExpectedNoExcept, "noexcept test failed");
+    }
+
+    {
+        using std::invoke;
+
+        auto res = invoke(callable, static_cast<Obj &&>(obj));
+        static_assert(std::is_same_v<decltype(res), ExpectedResT>,
+                      "result type is not the expected one");
+
+        constexpr bool nx{noexcept(invoke(callable, static_cast<Obj &&>(obj)))};
+        static_assert(nx == ExpectedNoExcept, "noexcept test failed");
+    }
+}
 
 struct TestInvoke {
 
@@ -128,87 +163,130 @@ struct TestInvoke {
         }
 
         {
-            auto callable = &Func::f_mut_lref_may_throw;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func &>);
-            static_assert(!noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func &>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_mut_lref_may_throw, f);
         }
 
         {
-            auto callable = &Func::f_mut_lref_noexcept;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func &>);
-            static_assert(noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func &>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_mut_lref_may_throw,
+                                       std::ref(f));
         }
 
         {
-            auto callable = &Func::f_mut_rref_may_throw;
-            auto res = tr::invoke(callable, std::move(f));
-            static_assert(res == tr::type_c<Func &&>);
-            static_assert(!noexcept(tr::invoke(callable, std::move(f))));
+            using res_t = tr::type_constant<Func &>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_mut_lref_noexcept, f);
         }
 
         {
-            auto callable = &Func::f_mut_rref_noexcept;
-            auto res = tr::invoke(callable, std::move(f));
-            static_assert(res == tr::type_c<Func &&>);
-            static_assert(noexcept(tr::invoke(callable, std::move(f))));
+            using res_t = tr::type_constant<Func &>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_mut_lref_noexcept, std::ref(f));
         }
 
         {
-            auto callable = &Func::f_mut_may_throw;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func>);
-            static_assert(!noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func &&>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_mut_rref_may_throw,
+                                       std::move(f));
         }
 
         {
-            auto callable = &Func::f_mut_noexcept;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func>);
-            static_assert(noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func &&>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_mut_rref_noexcept,
+                                       std::move(f));
         }
 
         {
-            auto callable = &Func::f_const_lref_may_throw;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func const &>);
-            static_assert(!noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_mut_may_throw, f);
         }
 
         {
-            auto callable = &Func::f_const_lref_noexcept;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func const &>);
-            static_assert(noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_mut_may_throw, std::ref(f));
         }
 
         {
-            auto callable = &Func::f_const_rref_may_throw;
-            auto res = tr::invoke(callable, std::move(f));
-            static_assert(res == tr::type_c<Func const &&>);
-            static_assert(!noexcept(tr::invoke(callable, std::move(f))));
+            using res_t = tr::type_constant<Func>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_mut_noexcept, f);
         }
 
         {
-            auto callable = &Func::f_const_rref_noexcept;
-            auto res = tr::invoke(callable, std::move(f));
-            static_assert(res == tr::type_c<Func const &&>);
-            static_assert(noexcept(tr::invoke(callable, std::move(f))));
+            using res_t = tr::type_constant<Func>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_mut_noexcept, std::ref(f));
         }
 
         {
-            auto callable = &Func::f_const_may_throw;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func const>);
-            static_assert(!noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func const &>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_const_lref_may_throw, f);
         }
 
         {
-            auto callable = &Func::f_const_noexcept;
-            auto res = tr::invoke(callable, f);
-            static_assert(res == tr::type_c<Func const>);
-            static_assert(noexcept(tr::invoke(callable, f)));
+            using res_t = tr::type_constant<Func const &>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_const_lref_may_throw,
+                                       std::ref(f));
+        }
+
+        {
+            using res_t = tr::type_constant<Func const &>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_const_lref_noexcept, f);
+        }
+
+        {
+            using res_t = tr::type_constant<Func const &>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_const_lref_noexcept,
+                                       std::ref(f));
+        }
+
+        {
+            using res_t = tr::type_constant<Func const &&>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_const_rref_may_throw,
+                                       std::move(f));
+        }
+
+        {
+            using res_t = tr::type_constant<Func const &&>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_const_rref_noexcept,
+                                       std::move(f));
+        }
+
+        {
+            using res_t = tr::type_constant<Func const>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_const_may_throw, f);
+        }
+
+        {
+            using res_t = tr::type_constant<Func const>;
+            constexpr bool nx{false};
+            test_member_ptr<res_t, nx>(&Func::f_const_may_throw, std::ref(f));
+        }
+
+        {
+            using res_t = tr::type_constant<Func const>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_const_noexcept, f);
+        }
+
+        {
+            using res_t = tr::type_constant<Func const>;
+            constexpr bool nx{true};
+            test_member_ptr<res_t, nx>(&Func::f_const_noexcept, std::ref(f));
         }
 
         {
@@ -217,21 +295,63 @@ struct TestInvoke {
                 return typename same_types_t::type{};
             };
 
-            auto callable = &Func::Data_;
+            auto memPtr = &Func::Data_;
 
-            auto sameTypes = same_types(tr::invoke(callable, f), f.Data_);
+            auto sameTypes = same_types(tr::invoke(memPtr, f), f.Data_);
             static_assert(sameTypes);
 
             // sameTypes is std::true_type. So I don't need to static_assert:
             // if I can assign to sameTypes, then same_types() returned
             // std::true_type.
-            sameTypes = same_types(tr::invoke(callable, std::as_const(f)),
+            sameTypes = same_types(tr::invoke(memPtr, std::as_const(f)),
                                    std::as_const(f).Data_);
-            sameTypes = same_types(tr::invoke(callable, std::move(f)),
+            sameTypes = same_types(tr::invoke(memPtr, std::move(f)),
                                    std::move(f).Data_);
             sameTypes =
-                same_types(tr::invoke(callable, std::move(std::as_const(f))),
+                same_types(tr::invoke(memPtr, std::move(std::as_const(f))),
                            std::move(std::as_const(f)).Data_);
+
+            sameTypes = same_types(tr::invoke(memPtr, std::ref(f)), f.Data_);
+            sameTypes = same_types(tr::invoke(memPtr, std::cref(f)),
+                                   std::as_const(f).Data_);
+        }
+
+        {
+            int i{};
+            auto std_res = std::invoke(FwdToTypePack{}, std::ref(i));
+            auto tr_res = tr::invoke(FwdToTypePack{}, std::ref(i));
+
+            // Ok: invoke only decays std::reference_wrapper<T> to T& when
+            // calling pointer to member functions/variables on T&
+            static_assert(std_res == tr_res);
+            static_assert(std_res ==
+                          tr::type_pack_c<std::reference_wrapper<int> &&>);
+        }
+
+        {
+            // std::reference_wrapper<> has a call operator.
+            auto f_ref = std::ref(f);
+
+            auto directRes = f_ref();
+            auto stdInvokeRes = std::invoke(f_ref);
+            auto trInvokeRes = tr::invoke(f_ref);
+
+            static_assert(directRes == stdInvokeRes);
+            static_assert(directRes == trInvokeRes);
+            static_assert(directRes == tr::type_c<Func &>);
+        }
+
+        {
+            // std::reference_wrapper<> has a call operator.
+            auto f_ref = std::cref(f);
+
+            auto directRes = f_ref();
+            auto stdInvokeRes = std::invoke(f_ref);
+            auto trInvokeRes = tr::invoke(f_ref);
+
+            static_assert(directRes == stdInvokeRes);
+            static_assert(directRes == trInvokeRes);
+            static_assert(directRes == tr::type_c<Func const &>);
         }
     }
 };
