f62f22a99a23a71f0b0c4c1d52d59dfa81e5c36e
Federico Abrignani <abrignani@lab3d.com>
2023-06-11 19:14:38 +0200

    Build system
    Update .gitignore.

--- include/dummy.cpp
+++ include/dummy.cpp
@@ -1,1 +1,43 @@
-int main() { }
+// dummy.cpp
+#include <tr/algorithm.h>
+#include <tr/as_array.h>
+#include <tr/at.h>
+#include <tr/combinator.h>
+#include <tr/detail/callable_wrapper_impl.h>
+#include <tr/detail/ebo.h>
+#include <tr/detail/flat_array.h>
+#include <tr/detail/literal_parser.h>
+#include <tr/detail/tuple_traits_utils.h>
+#include <tr/detail/type_traits.h>
+#include <tr/detail/utility.h>
+#include <tr/forward_as_base.h>
+#include <tr/indices_for.h>
+#include <tr/invoke.h>
+#include <tr/is_empty.h>
+#include <tr/is_valid.h>
+#include <tr/lazy_false.h>
+#include <tr/length.h>
+#include <tr/macros.h>
+#include <tr/overloaded.h>
+#include <tr/overload.h>
+#include <tr/tuple.h>
+#include <tr/tuple_protocol.h>
+#include <tr/tuple_protocol/built_in_array.h>
+#include <tr/tuple_protocol/std_integer_sequence.h>
+#include <tr/tuple_protocol/std_pair.h>
+#include <tr/tuple_protocol/std_tuple.h>
+#include <tr/type_constant.h>
+#include <tr/type_identity.h>
+#include <tr/type_pack.h>
+#include <tr/unimplemented.h>
+#include <tr/unpack.h>
+#include <tr/value_constant.h>
+#include <tr/value_sequence.h>
+#include <tr/view/drop_view.h>
+#include <tr/view/reverse_view.h>
+#include <tr/view/tuple_view.h>
+#include <tr/view/view_interface.h>
+
+int main() {
+    return 0;
+}
--- tests/invoke.cpp
+++ tests/invoke.cpp
@@ -5,6 +5,7 @@
 
 #include <functional>
 #include <type_traits>
+#include <utility>
 
 namespace {
 
